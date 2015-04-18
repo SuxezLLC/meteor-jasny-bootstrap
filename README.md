85ae4bf76963e4f160fe029fafc6842802f36b69
Roman Geber <rgeber@suxez.com>
2015-04-18 09:23:13 +0200

    Initial version containing jasny-bootstrap 3.1.3

---
+++ README.md
@@ -0,0 +1,16 @@
+# Jasny Bootstrap
+
+Wraps the [Jasny Bootstrap](http://jasny.github.io/bootstrap/) extensions in a meteor package. Current Version: 3.1.3.
+
+## Install
+
+    meteor add suxez:jansy-bootstrap
+
+## Authors
+
+ * [Jasny Bootstrap](http://jasny.github.io/bootstrap/): [Arnold Daniels](https://github.com/jasny)
+ * [Meteor Package](https://atmospherejs.com/suxez/jasny-bootstrap): [Suxez LLC](https://github.com/SuxezLLC)
+
+## License
+
+ * [Apache 2.0](https://github.com/jasny/bootstrap/blob/master/LICENSE)
---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+    name: 'suxez:jasny-bootstrap',
+    version: '1.0.0',
+    // Brief, one-line summary of the package.
+    summary: 'Jansy Bootstrap (3.1.3) extensions.',
+    // URL to the Git repository containing the source code for this package.
+    git: 'https://github.com/SuxezLLC/meteor-jasny-bootstrap',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
+});
+
+Package.onUse(function (api) {
+    api.versionsFrom('1.1.0.2');
+
+    var path = Npm.require('path');
+    var lib_path = path.join('lib');
+
+    api.add_files(path.join(lib_path, 'css', 'jasny-bootstrap.css'), 'client');
+    api.add_files(path.join(lib_path, 'js', 'jasny-bootstrap.js'), 'client');
+
+});
+
